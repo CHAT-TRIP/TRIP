c1bbb1d0bcd07ab20041a30d00fd5a1de1cba88f
dudafernanndes <dev.dudafernandes@gmail.com>
2025-05-22 11:43:08 -0300

    resolvendo erro para build

--- trip/src/components/Receitas.tsx
+++ trip/src/components/Receitas.tsx
@@ -8,6 +8,7 @@ const desktopBanners = [
   { src: '/anuncio-desktop-ccr.png', alt: 'Banner 2', width: 1140, height: 630 },
   /** { src: '/anuncio-desktop-ccr.png', alt: 'Banner 2', width: 1140, height: 630 },**/
   /** { src: '/anuncio-mobile-ccr.png', alt: 'Banner 2 Mobile', width: 364, height: 626 },**/
+  /** { src: '/anuncio-mobile-ccr.png', alt: 'Banner 2 Mobile', width: 364, height: 626 },**/
 
 ]
 
